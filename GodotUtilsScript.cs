85da90b7b3d2bb9951460a76e8a61ab70abb6ce9
ThreeRhinosInAnElephantCostume <myrealstemail@protonmail.com>
2022-02-11 13:13:34 +0100

    initial commit

---
+++ GodotUtilsScript.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using ExtraMath;
+using Godot;
+using Newtonsoft.Json;
+using static System.Math;
+using static Globals;
+using static Utils;
+using Expression = System.Linq.Expressions.Expression;
+
+[Tool]
+public class GlobalUtilsScript : Node
+{
+    ConcurrentQueue<Action> _toExec = new ConcurrentQueue<Action>();
+    void DequeDeferred()
+    {
+        System.Action action;
+        Assert(_toExec.TryDequeue(out action), "Error: queue desynchronization");
+        action();
+    }
+    public void QueueDeferred(System.Action action)
+    {
+        Assert(action != null);
+        _toExec.Enqueue(action);
+        CallDeferred(nameof(DequeDeferred));
+    }
+    public static GlobalUtilsScript GUS;
+    public GlobalUtilsScript()
+    {
+        GUS = this;
+    }
+}
